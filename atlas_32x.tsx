68aec691eed0c1d9c8ec144044e5be7406f24b16
Jeanno <hanzPhoks@users.noreply.github.com>
2021-03-18 21:17:13 +0100

    reeu ,1

---
+++ atlas_32x.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.4" tiledversion="1.4.3" name="atlas_32x" tilewidth="32" tileheight="32" tilecount="768" columns="48">
+ <image source="atlas_32x.png" width="1536" height="512"/>
+</tileset>
