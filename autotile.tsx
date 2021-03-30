c6f64615746d08f1a14c33f0d87234b3290b9339
hanzPhoks <hanzPhoks@users.noreply.github.com>
2021-03-30 16:35:17 +0000

    Deploying to gh-pages from master @ d2801ca92961a8385165d29ba93d4d22fedfc238 🚀

---
+++ autotile.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.4" tiledversion="1.4.3" name="autotile" tilewidth="32" tileheight="32" tilecount="432" columns="24">
+ <image source="../../../Downloads/Super_Retro_World_Exterior_pack_week12/Package/rpgmaker/autotile.png" width="768" height="576"/>
+</tileset>
