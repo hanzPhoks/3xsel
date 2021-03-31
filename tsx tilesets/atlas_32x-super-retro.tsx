270fe54e44cf84fd5acc8ae7119cbd226c2ce74a
Jeanno <hanzPhoks@users.noreply.github.com>
2021-03-31 22:51:03 +0200

    doc

--- tsx tilesets/atlas_32x-super-retro.tsx
+++ tsx tilesets/atlas_32x-super-retro.tsx
@@ -1,6 +1,6 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.4" tiledversion="1.4.3" name="atlas_32x-super-retro" tilewidth="32" tileheight="32" tilecount="1536" columns="96">
- <image source="atlas_32x-super-retro.png" width="3072" height="512"/>
+<tileset version="1.5" tiledversion="1.5.0" name="atlas_32x-super-retro" tilewidth="32" tileheight="32" tilecount="1536" columns="96">
+ <image source="../atlas_32x-super-retro.png" width="3072" height="512"/>
  <tile id="11">
   <properties>
    <property name="collides" type="bool" value="true"/>
